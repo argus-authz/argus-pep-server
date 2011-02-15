500901cd2289d5e4e490d288f3ecdeed03efea45
Valery Tschopp <valery.tschopp@switch.ch>
2011-02-15 15:07:11 +0000

    script to create symlinks

---
+++ src/script/build/emi-symlinks.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (c) Members of the EGEE Collaboration. 2006-2009.
+# See http://www.eu-egee.org/partners/ for details on the copyright holders.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+PEPD_NAME=pepd
+PEPD_HOME="/usr/share/argus/${PEPD_NAME}"
+
+prefix='../../../..'
+bin_prefix="$prefix/.."
+
+function create_symlink(){
+	if [ -e $2 ]; then
+		rm -rf $2
+	fi
+	ln -vs $1 $2
+}
+
+# conf
+create_symlink /etc/argus/$PEPD_NAME $PEPD_HOME/conf
+
+# lib
+create_symlink /var/lib/argus/$PEPD_NAME/lib $PEPD_HOME/lib
+
+# logs
+create_symlink /var/log/argus/$PEPD_NAME $PEPD_HOME/logs
+
+# doc
+create_symlink /usr/share/doc/argus/$PEPD_NAME $PAP_HOME/doc
+
