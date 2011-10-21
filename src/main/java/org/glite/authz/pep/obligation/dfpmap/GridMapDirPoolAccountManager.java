046bd49c0028f2835767ac3dc006bbad661b68a4
Valery Tschopp <valery.tschopp@switch.ch>
2011-10-21 14:58:32 +0000

    error message logged if a pool account is depleted (full).

--- src/main/java/org/glite/authz/pep/obligation/dfpmap/GridMapDirPoolAccountManager.java
+++ src/main/java/org/glite/authz/pep/obligation/dfpmap/GridMapDirPoolAccountManager.java
@@ -283,7 +283,7 @@ public class GridMapDirPoolAccountManager implements PoolAccountManager {
             }
             log.debug("Could not map to account {}", accountFile.getName());
         }
-
+        log.error("{} pool account is full. Failed to map {}", accountNamePrefix,subjectIdentifier);
         return null;
     }
 
