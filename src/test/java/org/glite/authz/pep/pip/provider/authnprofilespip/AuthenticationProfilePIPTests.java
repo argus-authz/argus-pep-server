6d2ab4a2954f4a89b6d315259ea4a813672f5690
Marco Caberletti <marco.caberletti@gmail.com>
2017-03-30 16:54:17 +0200

    Add PIP unit tests

--- src/test/java/org/glite/authz/pep/pip/provider/authnprofilespip/AuthenticationProfilePIPTests.java
+++ src/test/java/org/glite/authz/pep/pip/provider/authnprofilespip/AuthenticationProfilePIPTests.java
@@ -1,7 +1,153 @@
 package org.glite.authz.pep.pip.provider.authnprofilespip;
 
-public class AuthenticationProfilePIPTests extends TestSupport{
+import static eu.emi.security.authn.x509.impl.OpensslNameUtils.opensslToRfc2253;
+import static org.glite.authz.common.profile.CommonXACMLAuthorizationProfileConstants.DATATYPE_STRING;
+import static org.glite.authz.common.profile.CommonXACMLAuthorizationProfileConstants.DATATYPE_X500_NAME;
+import static org.glite.authz.common.profile.CommonXACMLAuthorizationProfileConstants.ID_ATTRIBUTE_SUBJECT_ID;
+import static org.glite.authz.common.profile.CommonXACMLAuthorizationProfileConstants.ID_ATTRIBUTE_VIRTUAL_ORGANIZATION;
+import static org.glite.authz.common.profile.CommonXACMLAuthorizationProfileConstants.ID_ATTRIBUTE_X509_SUBJECT_ISSUER;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 
-  
-  
+import java.util.Set;
+
+import org.glite.authz.common.model.Attribute;
+import org.glite.authz.common.model.Request;
+import org.glite.authz.common.model.Subject;
+import org.glite.authz.pep.pip.PIPProcessingException;
+import org.glite.authz.pep.pip.PolicyInformationPoint;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuthenticationProfilePIPTests extends TestSupport
+  implements AuthenticationProfilePIPConstants {
+
+  private PolicyInformationPoint pip;
+
+  @Before
+  public void setup() throws Exception {
+
+    AuthenticationProfilePDP pdp = new DefaultAuthenticationProfilePDP.Builder()
+      .authenticationPolicyFile(IGTF_WLCG_VO_CA_AP_FILE)
+      .trustAnchorsDir(TRUST_ANCHORS_DIR)
+      .policyFilePattern(IGTF_PROFILES_FILTER)
+      .build();
+
+    pip = new AuthenticationProfilePIP(pdp);
+    pip.start();
+  }
+
+  @After
+  public void teardown() throws Exception {
+
+    if (pip != null) {
+      pip.stop();
+    }
+  }
+
+  @Test
+  public void testVOWithSupportedProfile() throws PIPProcessingException {
+
+    Request request = createRequest(CLASSIC_DN, CLASSIC_CA, "atlas");
+
+    boolean result = pip.populateRequest(request);
+    assertThat(result, is(false));
+  }
+
+  @Test
+  public void testVOWithNotSupportedProfile() throws PIPProcessingException {
+
+    Request request = createRequest(CLASSIC_DN, IOTA_CA, TEST_VO);
+
+    boolean result = pip.populateRequest(request);
+    assertThat(result, is(true));
+
+    Set<Attribute> attributes = request.getSubjects()
+      .iterator()
+      .next()
+      .getAttributes();
+
+    assertThat(attributes, not(hasItem(createVoAttribute(TEST_VO))));
+    assertThat(attributes, hasItem(createIssuerAttribute(IOTA_CA)));
+  }
+
+  //
+  // @Test
+  // public void testPlainCertWithSupportedProfile() {
+  //
+  // }
+  //
+  // @Test
+  // public void testPlainCertWithNotSupportedProfile() {
+  //
+  // }
+  //
+  // @Test
+  // public void testRequestWithoutSubject() {
+  //
+  // }
+  //
+  // @Test
+  // public void testRequestWithoutIssuer() {
+  //
+  // }
+
+  private Request createRequest(String subjectDn, String issuerDn,
+    String voName) {
+
+    Request request = new Request();
+
+    Subject subject = new Subject();
+    Attribute subjectAttr = createSubjectAttribute(subjectDn);
+    subject.getAttributes()
+      .add(subjectAttr);
+
+    Attribute issuerAttr = createIssuerAttribute(issuerDn);
+    subject.getAttributes()
+      .add(issuerAttr);
+
+    Attribute voAttr = createVoAttribute(voName);
+    subject.getAttributes()
+      .add(voAttr);
+
+    request.getSubjects()
+      .add(subject);
+
+    return request;
+  }
+
+  private Attribute createVoAttribute(String voName) {
+
+    Attribute attr = new Attribute(ID_ATTRIBUTE_VIRTUAL_ORGANIZATION,
+      DATATYPE_STRING);
+    attr.getValues()
+      .add(voName);
+
+    return attr;
+  }
+
+  @SuppressWarnings("deprecation")
+  private Attribute createIssuerAttribute(String issuer) {
+
+    Attribute attr = new Attribute(ID_ATTRIBUTE_X509_SUBJECT_ISSUER,
+      DATATYPE_X500_NAME);
+    attr.getValues()
+      .add(opensslToRfc2253(issuer));
+
+    return attr;
+  }
+
+  @SuppressWarnings("deprecation")
+  private Attribute createSubjectAttribute(String subject) {
+
+    Attribute attr = new Attribute(ID_ATTRIBUTE_SUBJECT_ID, DATATYPE_X500_NAME);
+    attr.getValues()
+      .add(opensslToRfc2253(subject));
+
+    return attr;
+  }
+
 }
--- src/test/java/org/glite/authz/pep/pip/provider/authnprofilespip/TestSupport.java
+++ src/test/java/org/glite/authz/pep/pip/provider/authnprofilespip/TestSupport.java
@@ -7,24 +7,26 @@ import eu.emi.security.authn.x509.impl.OpensslNameUtils;
 public abstract class TestSupport {
 
   public static final String TRUST_ANCHORS_DIR = "src/test/resources/certificates";
-  public static final String IGTF_WLCG_VO_CA_AP_FILE =
-      "src/test/resources/vo-ca-ap/igtf-wlcg-vo-ca-ap";
-  
-  public static final String[] LHC_VOS = {"alice", "atlas", "cms", "lhcb"};
-  
+  public static final String IGTF_WLCG_VO_CA_AP_FILE = "src/test/resources/vo-ca-ap/igtf-wlcg-vo-ca-ap";
+
+  public static final String[] LHC_VOS = { "alice", "atlas", "cms", "lhcb" };
+
   public static final String TEST_VO = "test";
 
   public static final String IGTF_PROFILES_FILTER = "policy-igtf-*.info";
   public static final String ALL_POLICIES_FILTER = "policy-*.info";
-  
+
   public static final String CLASSIC_CA = "/C=IT/O=INFN/CN=INFN Certification Authority";
+  public static final String CLASSIC_DN = "/C=IT/O=INFN/OU=Personal Certificate/CN=User Tester";
+
   public static final String IOTA_CA = "/DC=ch/DC=cern/CN=CERN LCG IOTA Certification Authority";
   public static final String SLCS_CA = "/C=DE/O=DFN-Verein/OU=DFN-PKI/CN=DFN SLCS-CA";
   public static final String MICS_CA = "/C=NL/O=TERENA/CN=TERENA eScience Personal CA";
-  
+
   public static final String UNACCREDITED_CA = "/C=IT/O=Whatever/CN=Lonesome CA";
-  
+
   public X500Principal opensslDnToX500Principal(String dn) {
+
     String rfc2253Dn = OpensslNameUtils.opensslToRfc2253(dn);
     X500Principal principal = new X500Principal(rfc2253Dn);
     return principal;
